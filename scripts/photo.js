03255a96e11a9e61e4cb9f7c6e766810debfa83d
Давиденко Александр Арсеньевич <davaa@plant.elesy.com>
2023-11-24 12:43:47 +0700

    update

--- scripts/photo.js
+++ scripts/photo.js
@@ -1,14 +1,25 @@
-const photos = document.querySelectorAll(".info-small");
+const smallPhotos = document.querySelectorAll(".info-small");
+const largePhoto = document.querySelector("info__img-large");
 
-
-
-
-
-const fn = photos => {
-    photos.style.width = "1000px";
-    photos.style.height = "1000px";
+const openPhoto = photos => {
+    photos.classList.add("");
 };
 
-photos.forEach(photo => {
-    photo.addEventListener("click", () => fn(photo));
+const removeClass = elements => {
+    if(typeof elements === "object") {
+        elements.forEach(element => {
+            element.classList.remove("");
+        })
+    }
+    elements.classList.remove("");
+}
+
+const changeMainPhoto = (photo) => {
+    const currentPhoto = document.querySelector('.info__img-large');
+    if(currentPhoto.src === photo.src) return;
+    currentPhoto.setAttribute("src", photo.src);    
+}
+
+smallPhotos.forEach(photo => {
+    photo.addEventListener('click', () => changeMainPhoto(photo));
 });
