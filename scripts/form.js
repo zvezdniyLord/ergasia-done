42e325b711d965e114783bbf337ebbbe7c7f1bac
Давиденко Александр Арсеньевич <davaa@plant.elesy.com>
2023-11-29 09:10:53 +0700

    update test

--- scripts/form.js
+++ scripts/form.js
@@ -1,15 +1,11 @@
-const form = document.querySelector(".contact-form");
-
-const number = '79528917638';
-
-function sendToWhatsapp(text, phone) {
-    text = encodeURIComponent(text);
-    const url = `https://web.whatsapp.com/send?phone=${phone}&text=${text}&source=&data=`;
-    window.open(url);
-}
+const form = document.querySelector(".form");
 
 form.addEventListener("submit", e => {
     e.preventDefault();
-    const text = e.currentTarget.querySelector("input").value;
-    sendToWhatsapp(text, number);
-})
+    const formData = new FormData(form);
+    fetch("/send.php", {
+        method: "POST",
+        body: formData
+    })
+});
+
