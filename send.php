c30efc2836cafb45b0ee4df4c1863ec767971a37
Давиденко Александр Арсеньевич <davaa@plant.elesy.com>
2023-11-29 08:30:46 +0700

    update test

--- send.php
+++ send.php
@@ -1,33 +1,32 @@
-<?
-$token = '6775023082:AAEXfmuHyc9okoy8-5qxw2gAk2y5bnYEYw4';  
-$chat_id = '170195649';
 
-$arr = [
-	"Имя клиента: " => trim(strip_tags($_POST['user_name'])),
-	"Телефон клиента: " => trim(strip_tags($_POST['user_phone'])),
-	"Email клиента: " => trim(strip_tags($_POST['user_email'])),
-];
+<?php
 
-foreach ($arr as $key => $value) {
-	$txt .= "<br>" . $key . "<br>" . $value . "%0A";
+$token = "1094153697:AAFiLXXXXXLl0hRDsxBij1lddKydKxSSsOg04";
+
+$chat_id = "-40XXXX740";
+
+if ($_POST['act'] == 'order') {
+    $name = ($_POST['name']);
+    $phone = ($_POST['phone']);
+
+    $arr = array(
+        'Имя:' => $name,
+        'Телефон:' => $phone
+    );
+
+    foreach($arr as $key => $value) {
+        $txt .= "<b>".$key."</b> ".$value."%0A";
+    };
+
+    $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+    if ($sendToTelegram) {
+        alert('Спасибо! Ваша заявка принята. Мы свяжемся с вами в ближайшее время.');
+    }
+
+    else {
+        alert('Что-то пошло не так. ПОпробуйте отправить форму ещё раз.');
+    }
 }
 
-$url = "https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}";
-$proxy = "67.154.111.452:3128";
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-
-
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-
-curl_setopt($ch, CURLOPT_PROXY, $proxy);
-
-
-
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_HEADER, 1);
-$curl_scraped_page = curl_exec($ch);
-curl_close($ch);
+?>
